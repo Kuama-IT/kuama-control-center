ab94f13f0f33200c9c9959a97d4de712f9bc3975
Daniele <daniele@kuama.net>
2025-03-13 06:44:08 +0100

    fix: do not parallelize list all clients queries (supabase connections dies) cleanup: drizzle db client chore: format in hh:mm absence timeStart and timeEnd intervals

--- drizzle/drizzle-db.ts
+++ drizzle/drizzle-db.ts
@@ -4,28 +4,13 @@ import postgres from "postgres";
 import * as schema from "./schema";
 import * as relations from "./relations";
 
-// TODO clean up this mess https://github.com/orgs/supabase/discussions/21789#discussioncomment-11494102
-// TLDR; during development, multiple clients get instantiated, which causes the connection pool to be exhausted (due to hot reloading)
-const createDb = () => {
-  const client = postgres(serverEnv.databaseUrl, {
-    prepare: false,
-    max: 5,
-  });
-  const db = drizzle({
-    casing: "snake_case",
-    client,
-    logger: true,
-    schema: { ...schema, ...relations },
-  });
-
-  return db;
-};
-declare global {
-  var _db: ReturnType<typeof createDb> | undefined;
-}
-
-if (!global._db) {
-  global._db = createDb();
-}
-
-export const db = global._db;
+const client = postgres(serverEnv.databaseUrl, {
+  prepare: false,
+  connect_timeout: 3000,
+});
+export const db = drizzle({
+  casing: "snake_case",
+  client,
+  logger: true,
+  schema: { ...schema, ...relations },
+});
--- src/modules/k-clients/actions/k-client-list-all-action.ts
+++ src/modules/k-clients/actions/k-client-list-all-action.ts
@@ -5,8 +5,8 @@ import { inArray } from "drizzle-orm/sql/expressions/conditions";
 import { KClientListItem } from "@/modules/k-clients/k-clients-server";
 import { handleServerErrors } from "@/utils/server-action-utils";
 
-const kClientsListAllAction = async (): Promise<KClientListItem[]> => {
-  const clients = await db.query.kClients.findMany({
+const readClientsWithVat = async () => {
+  return await db.query.kClients.findMany({
     with: {
       kClientsVats: {
         with: {
@@ -16,35 +16,52 @@ const kClientsListAllAction = async (): Promise<KClientListItem[]> => {
     },
     orderBy: [asc(kClients.name)],
   });
-  // remove last trailing slash from the base url
+};
+type ClientWithVatList = Awaited<ReturnType<typeof readClientsWithVat>>;
 
-  const enhancedClients = await Promise.all(
-    clients.map(async ({ kClientsVats, ...client }) => {
-      const projects = await db
-        .select({ id: kProjects.id })
-        .from(kProjects)
-        .where(eq(kProjects.clientId, client.id));
+const clientProjectsAndTeam = async (clientId: number) => {
+  const projects = await db
+    .select({ id: kProjects.id })
+    .from(kProjects)
+    .where(eq(kProjects.clientId, clientId));
 
-      const employeesWorkingForClient = await db
-        .selectDistinct({ employeeId: kTeams.employeeId })
-        .from(kTeams)
-        .where(
-          inArray(
-            kTeams.projectId,
-            projects.map((project) => project.id),
-          ),
-        );
+  const employeesWorkingForClient = await db
+    .selectDistinct({ employeeId: kTeams.employeeId })
+    .from(kTeams)
+    .where(
+      inArray(
+        kTeams.projectId,
+        projects.map((project) => project.id),
+      ),
+    );
 
-      return {
-        ...client,
-        kVats: kClientsVats.map((it) => it.kVat),
-        avatarUrl: client.avatarUrl,
-        projectsCount: projects.length,
-        employeesWorkingForClientCount: employeesWorkingForClient.length,
-      };
-    }),
-  );
+  return {
+    projects,
+    employeesWorkingForClient,
+  };
+};
 
+const clientsWithProjectsAndTeam = async (baseClients: ClientWithVatList) => {
+  const clients: KClientListItem[] = [];
+  for (const { kClientsVats, ...client } of baseClients) {
+    const { projects, employeesWorkingForClient } = await clientProjectsAndTeam(
+      client.id,
+    );
+    clients.push({
+      ...client,
+      kVats: kClientsVats.map((it) => it.kVat),
+      avatarUrl: client.avatarUrl,
+      projectsCount: projects.length,
+      employeesWorkingForClientCount: employeesWorkingForClient.length,
+    });
+  }
+
+  return clients;
+};
+
+const kClientsListAllAction = async (): Promise<KClientListItem[]> => {
+  const clients = await readClientsWithVat();
+  const enhancedClients = await clientsWithProjectsAndTeam(clients);
   return enhancedClients.filter(
     (client) => client.projectsCount > 0 || client.name === "Kuama",
   );
