1a5c5f246dc04054f9b9e009085416320f2edfdf
Daniele <daniele@kuama.net>
2025-05-07 07:24:35 +0200

    fix(reports): add missing date range query params fix(k-access-token): move auth check outside of cached function

--- src/modules/k-access-tokens/k-access-tokens-server.ts
+++ src/modules/k-access-tokens/k-access-tokens-server.ts
@@ -2,10 +2,12 @@ import list from "@/modules/k-access-tokens/actions/k-access-token-list";
 import create from "@/modules/k-access-tokens/actions/k-access-token-create";
 import remove from "@/modules/k-access-tokens/actions/k-access-token-delete";
 import manage from "@/modules/k-access-tokens/actions/k-access-token-manage";
+import getNonExpiredToken from "@/modules/k-access-tokens/actions/k-access-token-get-not-expired";
 
 export const kAccessTokensServer = {
   list,
   create,
   remove,
   manage,
+  getNonExpiredToken,
 };
--- src/modules/k-platform-credentials/components/k-platform-credentials-card.tsx
+++ src/modules/k-platform-credentials/components/k-platform-credentials-card.tsx
@@ -4,7 +4,13 @@ import { Badge } from "@/components/ui/badge";
 import { SiJirasoftware, SiRedmine } from "react-icons/si";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
-import { FaArrowRight, FaDownload, FaLink, FaSync } from "react-icons/fa";
+import {
+  FaArrowRight,
+  FaDownload,
+  FaEye,
+  FaLink,
+  FaSync,
+} from "react-icons/fa";
 import {
   Dialog,
   DialogClose,
@@ -41,53 +47,8 @@ export const KPlatformCredentialsCard = ({
 }: {
   credentials: KPlatformCredentialsFullRead;
 }) => {
-  const [isPending, startTransition] = useTransition();
-  const router = useRouter();
-
-  const deleteCredentials = () => {
-    startTransition(async () => {
-      const res = await deleteKPlatformCredentials(credentials.id);
-      if (isFailure(res)) {
-        notifyError("Error during credentials deletion, check server logs");
-        return;
-      }
-
-      notifySuccess("Credentials deleted");
-      router.refresh();
-    });
-  };
-
-  const syncButtonToggle = useRef<HTMLButtonElement>(null);
-
-  const syncData = () => {
-    const { from, to } = range ?? {};
-    if (!from || !to) {
-      return;
-    }
-    notifySuccess("Data sync will continue in background");
-    startTransition(async () => {
-      await syncTimeSpentForClient(credentials.id, {
-        from,
-        to,
-      });
-    });
-    syncButtonToggle.current?.click();
-  };
-
   const canGenerateReport = credentials.platform === "easyredmine";
-  // TODO, we should just use location.origin, but Bless service cannot reach us in localhost...
-  const url = `https://kuama-control-center.vercel.app/reports/easyredmine/${credentials.id}`;
-  const reportName = credentials.kEmployee?.fullName ?? credentials.name;
-  const fileName = `${reportName.toLowerCase().replaceAll(" ", "-")}-report.pdf`;
 
-  const today = new Date();
-  const firstDateRange = startOfMonth(today);
-  const lastDateRange = endOfMonth(today);
-  const initialRange = {
-    from: firstDateRange,
-    to: lastDateRange,
-  };
-  const [range, setRange] = useState<DateRange | undefined>(initialRange);
   return (
     <div className="rounded-lg shadow-sm p-8 flex flex-col items-start gap-4 bg-background overflow-hidden group">
       <div className="flex items-center gap-4">
@@ -135,86 +96,216 @@ export const KPlatformCredentialsCard = ({
 
       <div className="flex gap-2 items-center">
         {canGenerateReport && (
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button ref={syncButtonToggle}>
-                <FaSync /> Sync timesheet
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent
-              className="w-auto p-0 flex flex-col gap-4"
-              align="start"
-            >
-              <Calendar
-                initialFocus
-                defaultMonth={range?.from}
-                numberOfMonths={2}
-                today={today}
-                mode="range"
-                onSelect={setRange}
-                selected={range}
-              />
-
-              <Separator />
-              <div className="flex items-center pb-4 justify-center">
-                <Button disabled={isPending} onClick={syncData}>
-                  Sync {format(range?.from ?? today, "dd-MM-yyyy")}
-                  <FaArrowRight />
-                  {format(range?.to ?? today, "dd-MM-yyyy")}{" "}
-                </Button>
-              </div>
-            </PopoverContent>
-          </Popover>
+          <SyncTimesheetButton credentialsId={credentials.id} />
         )}
 
         {canGenerateReport && (
-          <Link
-            className="w-full"
-            href={`/api/pdf?url=${url}&fileName=${fileName}`}
-            target="_blank"
-          >
-            <Button className="w-full">
-              <FaDownload />
-              Download timesheet
-            </Button>
-          </Link>
+          <DownloadTimesheetButton credentials={credentials} />
         )}
 
         <div className="ml-auto">
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button className="bg-destructive text-destructive-foreground uppercase flex">
-                <Trash />
-                Delete
-              </Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle className="text-destructive">
-                  Are you absolutely sure?
-                </DialogTitle>
-                <DialogDescription className="text-destructive">
-                  This action cannot be undone. This will permanently delete
-                  these credentials.
-                </DialogDescription>
-              </DialogHeader>
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type="button" variant="secondary">
-                    Nope, undo please
-                  </Button>
-                </DialogClose>
-                <Button
-                  disabled={isPending}
-                  onClick={() => deleteCredentials()}
-                >
-                  Yes
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
+          <DeleteButton credentialsId={credentials.id} />
         </div>
       </div>
     </div>
+  );
+};
+
+const DownloadTimesheetButton = ({
+  credentials,
+}: {
+  credentials: KPlatformCredentialsFullRead;
+}) => {
+  // TODO, we should just use location.origin, but Bless service cannot reach us in localhost...
+
+  const reportName = credentials.kEmployee?.fullName ?? credentials.name;
+  const fileName = `${reportName.toLowerCase().replaceAll(" ", "-")}-report.pdf`;
+
+  const syncButtonToggle = useRef<HTMLButtonElement>(null);
+
+  const today = new Date();
+  const firstDateRange = startOfMonth(today);
+  const lastDateRange = endOfMonth(today);
+  const initialRange = {
+    from: firstDateRange,
+    to: lastDateRange,
+  };
+  const [range, setRange] = useState<DateRange>(initialRange);
+
+  const from = range?.from ? format(range.from, "dd-MM-yyyy") : "";
+  const to = range?.to ? format(range.to, "dd-MM-yyyy") : "";
+  const url = `https://kuama-control-center.vercel.app/reports/easyredmine?credentialsId=${credentials.id}&from=${from}&to=${to}`;
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button ref={syncButtonToggle}>
+          <FaSync /> Download timesheet
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 flex flex-col gap-4" align="start">
+        <Calendar
+          initialFocus
+          defaultMonth={range?.from}
+          numberOfMonths={2}
+          today={today}
+          mode="range"
+          onSelect={(range) => {
+            if (range) {
+              setRange(range);
+            }
+          }}
+          selected={range}
+        />
+
+        <Separator />
+        <div className="flex items-center pb-4 justify-center gap-2">
+          {range?.from && range?.to && (
+            <>
+              <Link
+                className="w-full"
+                href={url}
+                target="_blank"
+                onClick={() => {
+                  syncButtonToggle.current?.click();
+                }}
+              >
+                <Button className="w-full">
+                  <FaEye />
+                  Preview timesheet {format(range.from, "dd-MM-yyyy")}
+                  <FaArrowRight />
+                  {format(range.to, "dd-MM-yyyy")}
+                </Button>
+              </Link>
+
+              <Link
+                className="w-full"
+                href={`/api/pdf?url=${url}&fileName=${fileName}`}
+                target="_blank"
+                onClick={() => {
+                  syncButtonToggle.current?.click();
+                }}
+              >
+                <Button className="w-full">
+                  <FaDownload />
+                  Download timesheet {format(range.from, "dd-MM-yyyy")}
+                  <FaArrowRight />
+                  {format(range.to, "dd-MM-yyyy")}
+                </Button>
+              </Link>
+            </>
+          )}
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const DeleteButton = ({ credentialsId }: { credentialsId: number }) => {
+  const [isPending, startTransition] = useTransition();
+
+  const router = useRouter();
+
+  const deleteCredentials = () => {
+    startTransition(async () => {
+      const res = await deleteKPlatformCredentials(credentialsId);
+      if (isFailure(res)) {
+        notifyError("Error during credentials deletion, check server logs");
+        return;
+      }
+
+      notifySuccess("Credentials deleted");
+      router.refresh();
+    });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-destructive text-destructive-foreground uppercase flex">
+          <Trash />
+          Delete
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="text-destructive">
+            Are you absolutely sure?
+          </DialogTitle>
+          <DialogDescription className="text-destructive">
+            This action cannot be undone. This will permanently delete these
+            credentials.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Nope, undo please
+            </Button>
+          </DialogClose>
+          <Button disabled={isPending} onClick={() => deleteCredentials()}>
+            Yes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+const SyncTimesheetButton = ({ credentialsId }: { credentialsId: number }) => {
+  const [isPending, startTransition] = useTransition();
+  const syncButtonToggle = useRef<HTMLButtonElement>(null);
+
+  const syncData = () => {
+    const { from, to } = range ?? {};
+    if (!from || !to) {
+      return;
+    }
+    notifySuccess("Data sync will continue in background");
+    startTransition(async () => {
+      await syncTimeSpentForClient(credentialsId, {
+        from,
+        to,
+      });
+    });
+    syncButtonToggle.current?.click();
+  };
+
+  const today = new Date();
+  const firstDateRange = startOfMonth(today);
+  const lastDateRange = endOfMonth(today);
+  const initialRange = {
+    from: firstDateRange,
+    to: lastDateRange,
+  };
+  const [range, setRange] = useState<DateRange | undefined>(initialRange);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button ref={syncButtonToggle}>
+          <FaSync /> Sync timesheet
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0 flex flex-col gap-4" align="start">
+        <Calendar
+          initialFocus
+          defaultMonth={range?.from}
+          numberOfMonths={2}
+          today={today}
+          mode="range"
+          onSelect={setRange}
+          selected={range}
+        />
+
+        <Separator />
+        <div className="flex items-center pb-4 justify-center">
+          <Button disabled={isPending} onClick={syncData}>
+            Sync {format(range?.from ?? today, "dd-MM-yyyy")}
+            <FaArrowRight />
+            {format(range?.to ?? today, "dd-MM-yyyy")}{" "}
+          </Button>
+        </div>
+      </PopoverContent>
+    </Popover>
   );
 };
